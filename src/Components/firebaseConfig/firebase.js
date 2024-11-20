b91f25494b112a9e3a802c55f4eec43ad365e4bc
MoSu1248 <mohammedsuhailr2@gmail.com>
2024-11-20 12:59:28 +0200

    Second commit

--- src/Components/firebaseConfig/firebase.js
+++ src/Components/firebaseConfig/firebase.js
@@ -3,8 +3,8 @@ import {getAuth} from "firebase/auth"
 import { getFirestore } from '@firebase/firestore'
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC_bgtloxNM7xxt15Zgst4BkZQSlbANz8c",
-  authDomain: "truckingwellness-714f3.firebaseapp.com",
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
   databaseURL: "https://truckingwellness-714f3-default-rtdb.firebaseio.com",
   projectId: "truckingwellness-714f3",
   storageBucket: "truckingwellness-714f3.appspot.com",
